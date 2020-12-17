4dfda711917abd97fefccedee81fcf0de21ef2b6
Meena Bassem <mpdotf@gmail.com>
2020-12-17 15:54:41 +0200

    Updated style proptypes to Proptypes.any

--- src/components/label/index.js
+++ src/components/label/index.js
@@ -43,7 +43,7 @@ export default class Label extends PureComponent {
       y1: PropTypes.number,
     }),
 
-    style: Animated.Text?.propTypes?.style,
+    style: PropTypes.any,//Animated.Text?.propTypes?.style,
     label: PropTypes.string,
   };
 
